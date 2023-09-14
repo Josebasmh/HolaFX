147ae0c130489a588ee9ab5bacaa979b8dc6da8d
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-09-14 09:44:10 +0200

    MOD Main.java
    se modificó el main.java tras no completarse de serie

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,17 @@
 package application;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void start(Stage stage) {
+	stage.setTitle("Stage with a Button in the Scene"); 
+	Group root = new Group(new Button("Hello"));
+	Scene scene = new Scene(root); 
+	stage.setScene(scene);
+	stage.show();
 	}
-
 }
