3636c78cf4639e902233e63a1b9f53f7670fdcaa
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-09-14 10:01:56 +0200

    ADD Vbox 'ejercicio A'

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,14 +4,19 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
 	public void start(Stage stage) {
-	stage.setTitle("Stage with a Button in the Scene"); 
-	Group root = new Group(new Button("Hello"));
-	Scene scene = new Scene(root); 
+	stage.setTitle("HolaFX"); 
+	Group root = new Group(new Button("Botón"));
+	VBox vb = new VBox();
+	Text msg= new Text("Ejercicio A");
+	vb.getChildren().add(msg);
+	Scene scene = new Scene(vb,300,200); 
 	stage.setScene(scene);
 	stage.show();
 	}
