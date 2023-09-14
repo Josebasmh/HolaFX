3e3a3892fbde30360f404b008a26dd635f53f7c7
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-09-14 09:46:38 +0200

    MOD main.java
    solucionado la base del programa

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
 
+import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application{
 
 	public void start(Stage stage) {
 	stage.setTitle("Stage with a Button in the Scene"); 
@@ -13,5 +14,8 @@ public class Main {
 	Scene scene = new Scene(root); 
 	stage.setScene(scene);
 	stage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
